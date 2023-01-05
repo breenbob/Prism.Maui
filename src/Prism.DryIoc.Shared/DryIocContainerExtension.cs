938b9cdeac80be805b7ff8bfdef1865461dcad9f
Conor Breen <conor.breen@civica.co.uk>
2023-01-05 13:48:08 +0000

    Re-add call to disable fast expression compilation

--- src/Prism.DryIoc.Shared/DryIocContainerExtension.cs
+++ src/Prism.DryIoc.Shared/DryIocContainerExtension.cs
@@ -24,7 +24,7 @@ namespace Prism.DryIoc
                                                          .With(Made.Of(FactoryMethod.ConstructorWithResolvableArguments))
                                                          .WithFuncAndLazyWithoutRegistration()
                                                          .WithTrackingDisposableTransients()
-                                                        // .WithoutFastExpressionCompiler()
+                                                         .WithoutFastExpressionCompiler()
                                                          .WithFactorySelector(Rules.SelectLastRegisteredFactory());
 
         /// <summary>
